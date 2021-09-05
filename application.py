d454e3337a278a51c59950a28eae8e1638be9642
Andrew <svt.andrey1631@gmail.com>
2021-09-05 17:31:59 +0300

    fill start window and create transition to the main window

--- application.py
+++ application.py
@@ -19,16 +19,23 @@ class Application:
                 self.start_events()
                 self.start_update()
                 self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            else:
+                self.running = False
             self.clock.tick(fps)
         pygame.quit()
         sys.exit()
 
-    def draw_text(self, screen, size, color, name, text, pos):
+    def draw_text(self, screen, size, color, name, text, pos, centered=False):
         font = pygame.font.SysFont(font_name, size, )
         my_text = font.render(text, False, color)
         text_size = my_text.get_size()
-        pos[0] = pos[0] - text_size[0] // 2
-        pos[1] = pos[1] - text_size[1] // 2
+        if centered:
+            pos[0] = pos[0] - text_size[0] // 2
+            pos[1] = pos[1] - text_size[1] // 2
         screen.blit(my_text, pos)
 
     def start_events(self):
@@ -37,15 +44,58 @@ class Application:
                 self.running = False
             if ev.type == pygame.KEYDOWN and ev.key == pygame.K_SPACE:
                 self.state = 'playing'
+
     def start_update(self):
         pass
 
     def start_draw(self):
         self.screen.fill((0, 0, 0))
-        text = 'Push space bar'
+        text1 = 'Push space bar'
+        text2 = 'One player only'
+        text3 = 'high score'
         pos_tx = width // 2
         pos_ty = height // 2
-        self.draw_text(self.screen, start_text_size, (185, 134, 27), font_name, text, [pos_tx, pos_ty])
+        self.draw_text(self.screen, start_text_size, (185, 134, 27), font_name, text1, [pos_tx, pos_ty], True)
+        self.draw_text(self.screen, start_text_size, (51, 153, 153), font_name, text2, [pos_tx, pos_ty+50], True)
+        self.draw_text(self.screen, start_text_size, (242, 243, 222), font_name, text3, [5, 3])
         pygame.display.update()
+
+    def playing_events(self):
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                self.running = False
+
+    def playing_update(self):
+        pass
+
+    def playing_draw(self):
+        self.screen.fill((37, 213, 149))
+        pygame.display.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
